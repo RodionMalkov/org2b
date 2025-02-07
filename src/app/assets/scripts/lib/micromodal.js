4e78eb50741320f4e9b1ff2d37ae0864855699a4
RodionMalkov <rodionmalkov@gmail.com>
2025-02-07 18:16:57 +0500

    [modal] upd modal trigger

--- src/app/assets/scripts/lib/micromodal.js
+++ src/app/assets/scripts/lib/micromodal.js
@@ -3,5 +3,7 @@ import MicroModal from 'micromodal'
 MicroModal.init({
   disableScroll: true,
   awaitOpenAnimation: true,
-  awaitCloseAnimation: true
+  awaitCloseAnimation: true,
+  openTrigger: 'data-modal-open',
+  closeTrigger: 'data-modal-close'
 })
