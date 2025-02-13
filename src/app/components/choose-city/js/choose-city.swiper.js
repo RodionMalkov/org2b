8ffff80870a9cf778d70698dbdb4a9549fa27a06
RodionMalkov <rodionmalkov@gmail.com>
2025-02-13 09:50:14 +0500

    [choose-city] clean swiper script

--- src/app/components/choose-city/js/choose-city.swiper.js
+++ src/app/components/choose-city/js/choose-city.swiper.js
@@ -5,19 +5,20 @@ import 'swiper/css/navigation'
 import 'swiper/css/scrollbar'
 
 export default function handlerChoseCitySwiper() {
+  const chooseCitySwipers = document.querySelectorAll(
+    '.choose-city__swiper.swiper'
+  )
   let aboutGallery = []
-  document
-    .querySelectorAll('.choose-city__swiper.swiper')
-    .forEach((swiperElement, index) => {
-      const scrollbar = swiperElement.querySelector('.scrollbar')
-      console.log(scrollbar)
 
-      aboutGallery[index] = new Swiper(swiperElement, {
-        modules: [Navigation, Scrollbar],
-        slidesPerView: 'auto',
-        scrollbar: {
-          el: scrollbar
-        }
-      })
+  chooseCitySwipers.forEach((swiperElement, index) => {
+    const scrollbar = swiperElement.querySelector('.scrollbar')
+
+    aboutGallery[index] = new Swiper(swiperElement, {
+      modules: [Navigation, Scrollbar],
+      slidesPerView: 'auto',
+      scrollbar: {
+        el: scrollbar
+      }
     })
+  })
 }
