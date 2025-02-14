93073d62e27261c523fa890159b7cbcc532ee537
RodionMalkov <rodionmalkov@gmail.com>
2025-02-14 17:05:31 +0500

    [micromodal] add onClose functions

--- src/app/assets/scripts/lib/micromodal.js
+++ src/app/assets/scripts/lib/micromodal.js
@@ -5,5 +5,17 @@ MicroModal.init({
   awaitOpenAnimation: true,
   awaitCloseAnimation: true,
   openTrigger: 'data-modal-open',
-  closeTrigger: 'data-modal-close'
+  closeTrigger: 'data-modal-close',
+  onClose: () => {
+    const navBtn = document.querySelectorAll('button[data-modal-open]')
+    if (!navBtn) {
+      return
+    }
+
+    navBtn.forEach(button => {
+      if (button.classList.contains('active')) {
+        button.classList.remove('active')
+      }
+    })
+  }
 })
